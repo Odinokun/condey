bb7a6adf5d1d613c5a738d6a4505399303a82809
odinokun <odinokun@gmail.com>
2018-04-17 18:07:38 +0300

    did why we

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -1,15 +1,29 @@
 module.exports = function() {
 
   // begin Slick slider
+    // begin top-slider
+      $('#top-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        autoplay: true,
+        cssEase: 'linear'
+      });
+    // end top-slider
 
-  $('#top-slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    autoplay: true,
-    cssEase: 'linear'
-  });
+    // begin sale-slider
+      $('#sale-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        autoplay: true,
+        cssEase: 'linear'
+      });
+    // end sale-slider
+
+
 
   // end Slick slider
 
